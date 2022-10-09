a70da9c9029b9df0ac8d864ace70cefe5dd96659
mentalblood <neceporenkostepan@gmail.com>
2022-10-09 15:34:08 +0300

    more correct package files

--- setup.py
+++ setup.py
@@ -4,17 +4,39 @@ from setuptools import setup, find_packages
 if __name__ == '__main__':
 
 	setup(
+
 		name='sepasync',
-		version='1.3.2',
-		description='Fast, working tool for downloading entries from SEP',
-		long_description=open('README.md').read(),
-		long_description_content_type='text/markdown',
-		author='mentalblood',
+		version='1.0.0',
+		python_requires='>=3.10',
 		install_requires=[
 			'bs4',
 			'loguru',
 			'httpx',
+			'mutagen',
 			'python_version >= "3.10"'
 		],
+		keywords=['downloader'],
+		url='https://github.com/MentalBlood/sepasync',
+
+		description='Fast, working tool for downloading entries from Stanford Encyclopedia of Philosophy',
+		long_description=open('README.md').read(),
+		long_description_content_type='text/markdown',
+
+		classifiers=[
+			'Development Status :: 5 - Production/Stable',
+			'Intended Audience :: End Users/Desktop'
+			'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+			'Typing :: Typed',
+			'Operating System :: OS Independent',
+			'Programming Language :: Python :: 3.10',
+			'License :: OSI Approved :: BSD License'
+		],
+
+		author='mentalblood',
+		author_email='neceporenkostepan@gmail.com',
+		maintainer='mentalblood',
+		maintainer_email='neceporenkostepan@gmail.com',
+
 		packages=find_packages()
+
 	)
